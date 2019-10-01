470cf9fafd4eece5a9c235850129d48f783f71d5
GSkyler <gaoskyler@gmail.com>
2019-10-01 14:03:13 -0500

    Commit test

--- app/src/main/java/com/example/soccerstattrackerproject/MainActivity.java
+++ app/src/main/java/com/example/soccerstattrackerproject/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.soccerstattrackerproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView playerListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerListView = findViewById(R.id.playerListView);
     }
 }
