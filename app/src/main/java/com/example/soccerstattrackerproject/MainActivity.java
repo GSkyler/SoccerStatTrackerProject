03ac094a2a2ad21e73a54eb3598bb21b73731e9d
GSkyler <gaoskyler@gmail.com>
2019-10-03 13:48:33 -0500

    Commit test 3

--- app/src/main/java/com/example/soccerstattrackerproject/MainActivity.java
+++ app/src/main/java/com/example/soccerstattrackerproject/MainActivity.java
@@ -3,17 +3,23 @@ package com.example.soccerstattrackerproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView playerListView;
+    private ArrayAdapter adapter;
+    private String[] playerNames;
+    private int MUB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        playerNames = new String[]{"A", "B"};
         playerListView = findViewById(R.id.playerListView);
+//        adapter = new ArrayAdapter<String>(this, R.id.playerListView, playerNames);
     }
 }
