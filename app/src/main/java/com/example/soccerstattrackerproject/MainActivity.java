fc4c263f96bf111ace997cf9ef5ee7abf781ad45
Sapna1234 <nattuvetty1459@students.d211.org>
2019-10-04 14:32:45 -0500

    Added the DATA base but dont compleatly work

--- app/src/main/java/com/example/soccerstattrackerproject/MainActivity.java
+++ app/src/main/java/com/example/soccerstattrackerproject/MainActivity.java
@@ -2,23 +2,48 @@ package com.example.soccerstattrackerproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView playerListView;
     private ArrayAdapter adapter;
-    private String[] playerNames;
+    private TextView IDK;
+
+    private Team team;
+    DatabaseHelper MyDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        playerNames = new String[]{"A", "B"};
+        MyDb = new DatabaseHelper(this);
+        IDK = findViewById(R.id.textView);
+        team = new Team();
         playerListView = findViewById(R.id.playerListView);
-//        adapter = new ArrayAdapter<String>(this, R.id.playerListView, playerNames);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, team.getPlayerNames());
+        playerListView.setAdapter(adapter);
+
+    }
+
+    public void ADDtoDb(View View){
+        boolean Good = MyDb.insertData("Skyler", "7.5", "Goal", "1", "1");
+        if(Good == true){
+            IDK.setText("TRUE");
+        }
+        else{
+            IDK.setText("NOT TRUE");
+        }
+
     }
 }
+
+
